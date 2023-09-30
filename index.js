c711790d0963c4b097a4e2a7b40766eb8e8da92a
Sundeep <sundeep.kaler@gmail.com>
2023-09-30 09:22:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driversArray, driverName){
+    return driversArray.filter(function(driver) {return driver.toLowerCase() === driverName.toLowerCase()});
+}
+
+function fuzzyMatch(driversArray, fuzzyName){
+    return driversArray.filter(function (driver) {return (driver.includes(fuzzyName) && (driver.indexOf(fuzzyName) === 0))})
+}
+
+function matchName(driversArray, driverName){
+    return driversArray.filter(function(driver) {return driver.name === driverName});
+}
